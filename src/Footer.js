bfabdfdf806fe5eb7c2a434b8eb200e5c915c285
jrparente <jrparente@gmail.com>
2022-10-06 10:58:24 +0100

    Added link to GitHub on Footer

--- src/Footer.js
+++ src/Footer.js
@@ -11,6 +11,14 @@ export default function Footer() {
           rel="noopener noreferrer"
         >
           Joana Parente
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/jrparente/shecodes-react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
         </a>
       </p>
     </footer>
