b0b748cb4553997be3e0e4b306c658d5ccb5515d
quill2code <denisehjohnson@yahoo.com>
2025-07-20 12:13:15 -0700

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -6,28 +6,14 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        This project was coded by{" "}
-        <a
-          href="https://www.joanaramosparente.pt/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Joana Parente
-        </a>{" "}
-        it's{" "}
-        <a
-          href="https://github.com/jrparente/shecodes-react-weather-app"
+        This project was coded by Denise Johnson
+    <a
+          href="https://github.com/quill2code/shecodes-react-weather-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://shecodes-react-weather-app-jp.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      
           hosted on Netlify
         </a>
         .
